8197d5fad8cf30122efff9ace5487509cd9c566d
Sheikh Ammar <sheikhammar568@gmail.com>
2022-12-21 10:07:31 +0500

    Add middleware Sanctum Token Work in Larvel Task

--- Laravel-Tasks/app/Http/Controllers/UserController.php
+++ Laravel-Tasks/app/Http/Controllers/UserController.php
@@ -4,12 +4,63 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
-        
+        $request->validate([
+            'name' => 'required|alpha',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('myToken')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                "Message" => "Provide Credentials Are Incorrect !",
+            ], 401);
+        }
+
+        $token = $user->createToken('myToken')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token,
+        ], 200);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return response([
+            'Message' => 'Successfully LogOut !',
+            'Status' => 200,
+        ]);
     }
 }
--- Laravel-Tasks/routes/api.php
+++ Laravel-Tasks/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\GradesController;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,27 +18,42 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+// ================================================ //
+//               PROTECTED ROUTES                   //
+// ================================================ //
+Route::middleware('auth:sanctum')->group(function () {
+    // COURSE
+    Route::post('/course', [CourseController::class, 'store']);
+    Route::put('/course/{id}', [CourseController::class, 'update']);
+    Route::delete('/course/{id}', [CourseController::class, 'destroy']);
+
+    // GRADES
+    Route::post('/grades', [GradesController::class, 'store']);
+    Route::put('/grades/{id}', [GradesController::class, 'update']);
+    Route::delete('/grades/{id}', [GradesController::class, 'destroy']);
+
+    // STUDENTS
+    Route::post('/students', [StudentController::class, 'store']);
+    Route::put('/students/{id}', [StudentController::class, 'update']);
+    Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+
+    // AUTH
+    Route::post('/logout', [UserController::class, 'logout']);
 });
 
 
+// ================================================ //
+//                   PUBLIC ROUTES                  //
+// ================================================ //
+// AUTH
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
 // COURSE ROUTES
 Route::get('/course', [CourseController::class, 'index']);
 Route::get('/course/{id}', [CourseController::class, 'show']);
-Route::post('/course', [CourseController::class, 'store']);
-Route::put('/course/{id}', [CourseController::class, 'update']);
-Route::delete('/course/{id}', [CourseController::class, 'destroy']);
-
 // GRADES ROUTES
 Route::get('/grades', [GradesController::class, 'index']);
 Route::get('/grades/{id}', [GradesController::class, 'show']);
-Route::post('/grades', [GradesController::class, 'store']);
-Route::put('/grades/{id}', [GradesController::class, 'update']);
-Route::delete('/grades/{id}', [GradesController::class, 'destroy']);
-
 // STUDENTS ROUTES
 Route::get('/students', [StudentController::class, 'index']);
-Route::post('/students', [StudentController::class, 'store']);
-Route::put('/students/{id}', [StudentController::class, 'update']);
-Route::delete('/students/{id}', [StudentController::class, 'destroy']);
