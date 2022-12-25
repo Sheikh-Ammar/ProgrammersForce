8065741aa97de00f5d79349d81bab1933d831af5
Sheikh Ammar <sheikhammar568@gmail.com>
2022-12-25 17:04:57 +0500

    Remove All Laravel Practice Work From Master

--- Laravel-Practice/storage/framework/views/2e79aa5351cb556015614554792db069e816fb9c.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH C:\xampp\htdocs\ProgrammerForce\Laravel-Practice\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
--- Laravel-Practice/storage/framework/views/a1e1d36b5352e46fad8bf134f3a65267e0a4ddb9.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH C:\xampp\htdocs\ProgrammerForce\Laravel-Practice\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
