f7c869909466bb5e7f7cfb550a855336173e8e88
Sheikh Ammar <sheikhammar568@gmail.com>
2022-12-25 17:00:45 +0500

    Make Another Branch For Laravel practice

---
+++ Laravel-Practice/storage/framework/views/9b91e95431ccd19f35d65f45d5bee9a26ac51d09.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH C:\xampp\htdocs\ProgrammerForce\Laravel-Practice\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/table.blade.php ENDPATH**/ ?>
